04d5078f51a5c7db60da63b7dd4d05295382c492
Jarnail Singh <96626033+jsr517@users.noreply.github.com>
2023-01-15 16:38:33 -0800

    S3 bucket modified

--- fetch_image_from_s3.py
+++ fetch_image_from_s3.py
@@ -5,11 +5,11 @@ import boto3
 s3 = boto3.client('s3')
 
 def lambda_handler(event, context):
-    bucket_name = "xyz"
-    file_name = "abc.jpg"
+    bucket_name = "MyBucket"
+    file_name = "mypic.jpg"
     fileObj = s3.get_object(Bucket=bucket_name, Key=file_name)
     file_content = fileObj["Body"].read()
-    # print
+    # print 
     return {
         "statusCode": 200,
         "headers": {
